61b8255e7acac4f6ae66176d2f6eb0d9bd64bfeb
DemetriusLeonardoBantim <demetriusbantimvas@gmail.com>
2023-11-12 20:12:36 -0300

    feature: create interface repository

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -4,5 +4,5 @@ import { Category } from './category.entity'
 import { Uuid } from '../../shared/domain/value-objects/uuid.vo'
 
 export interface ICategoryRepository extends IRepository<Category, Uuid> {
-
+  
 }

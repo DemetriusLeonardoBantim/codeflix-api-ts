deb5dfd4570a6e3b35452b20c8adb644c7577098
DemetriusLeonardoBantim <demetriusbantimvas@gmail.com>
2023-11-12 18:15:18 -0300

    test: change config tests

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -1,12 +1,19 @@
+import { EntityValidationError } from  '../../../shared/validators/validation.error'
+import { Uuid } from '../../../shared/domain/value-objects/uuid.vo'
 import { Category } from "../category.entity"
 
+
 describe('Category Unit Tests', () => {
+  let validateSpy: any
+  beforeEach(() => {
+    validateSpy = jest.spyOn(Category, "validate")
+  })
+
   test('Should create a category with default values', () => {
     const category = new Category({
       name: 'Movie',
-      category_id: '123',
     })
-    expect(category.category_id).toBe('123')
+    expect(category.category_id).toBeInstanceOf(Uuid)
     expect(category.name).toBe('Movie')
     expect(category.description).toBeNull()
     expect(category.is_active).toBeTruthy()
@@ -17,11 +24,25 @@ describe('Category Unit Tests', () => {
     const created_at = new Date()
     const category = new Category({
       name: 'Movie',
-      category_id: '123',
       is_active: false,
       created_at,
       description: 'Description movie'
     })
-
+    expect(category.category_id).toBeInstanceOf(Uuid)
   })
+})
+
+describe('Category Validator', () => {
+/*   describe('create command' , () => {
+    new EntityValidationError({
+      name: ["name is required"],
+    })
+    try {
+      Category.create({
+        name: ""
+      })
+    } catch(e) {
+      console.log(e)
+    }
+  }) */
 })

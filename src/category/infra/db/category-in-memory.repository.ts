87b3dbfb01f804bde26e88ccb63ba101ccdb9523
DemetriusLeonardoBantim <demetriusbantimvas@gmail.com>
2023-11-14 11:59:05 -0300

    feature: create category repository

---
+++ src/category/infra/db/category-in-memory.repository.ts
@@ -0,0 +1,8 @@
+import { Category } from "src/category/domain/category.entity";
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
+import { InMemoryRepository } from '../../../shared/infra/db/in-memory/in-memory.repository'
+
+
+export class CategoryInMemoryRepository extends InMemoryRepository<Category, Uuid> {
+
+}

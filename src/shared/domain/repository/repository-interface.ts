3f4b69a86547b68ba36416ab9e7f479dd2d5d64f
DemetriusLeonardoBantim <demetriusbantimvas@gmail.com>
2023-11-18 12:32:14 -0300

    refactor: change param in repository

--- src/shared/domain/repository/repository-interface.ts
+++ src/shared/domain/repository/repository-interface.ts
@@ -7,8 +7,8 @@ import { SearchResult } from "./search-result"
 export interface IRepository<E extends Entity, EntityId extends ValueObject> {
   insert(entity: E): Promise<void>
   dulkInsert(entities: E[]): Promise<void>
-  update(entity: E[]): Promise<void>
-  delete(entity_id: EntityId): Promise<void>
+  update(entity: E): Promise<void>;
+  delete(entity_id: EntityId): Promise<void>;
 
   findById(entity_id: EntityId): Promise<E>
   findAll(): Promise<E[]>

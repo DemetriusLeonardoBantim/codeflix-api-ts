34cbf89f24648853ae60f43849f7e5d715b061e0
DemetriusLeonardoBantim <demetriusbantimvas@gmail.com>
2023-11-18 12:50:00 -0300

    feature: add new method in category sequelize repository

--- src/category/domain/category.repository.ts
+++ src/category/domain/category.repository.ts
@@ -1,8 +1,20 @@
 import { extend } from 'lodash'
 import { ISeartchableRepository } from '../../shared/domain/repository/repository-interface'
+import { SearchParams } from '../../shared/domain/repository/search-params'
+import { SearchResult } from '../../shared/domain/repository/search-result'
 import { Category } from './category.entity'
 import { Uuid } from '../../shared/domain/value-objects/uuid.vo'
 
-export interface ICategoryRepository extends ISeartchableRepository<Category, Uuid> {
+export type CategoryFilter = string;
+
+export class CategorySearchParams extends SearchParams<CategoryFilter> {
+
+}
+
+export class CategorySearchResult extends SearchResult<Category> {
+  
+}
+
+export interface ICategoryRepository extends ISeartchableRepository<Category, Uuid, CategoryFilter, CategorySearchParams, CategorySearchResult> {
   
 }
